a906235b12c74c6ee847de6ff2b45eb2e825dbb1
bilalKhan2-cell <81862337+bilalKhan2-cell@users.noreply.github.com>
2024-01-26 16:12:36 +0500

    Update marks.blade.php

--- resources/views/projectlead/marks.blade.php
+++ resources/views/projectlead/marks.blade.php
@@ -9,3 +9,31 @@
 @endsection
 
 @section('content')
+    <div class="row">
+        <div class="col s12">
+            <table class="table table-hover striped">
+                <thead>
+                    <tr>
+                        <th>Team ID</th>
+                        <th>Thesis Title</th>
+                        <th>Marks</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>TEAM-{{$thesis_details->team_id}}</td>
+                        <td>{{$thesis_details->thesis_title}}</td>
+                        <td>
+                            @if($status==0)
+                                <span class='red-text'>Your Thesis Is Still In Approval Process</span>
+                            @elseif($status==2)
+                                <span class='blue-text'>Your Thesis Is Approved But Marks Assigning Is Still In Process</span>
+                            @else 
+                                <span class='green-text'>{{ $thesis_details->marks }} out of 200</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
